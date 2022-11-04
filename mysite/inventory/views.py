58e73833b431b51ac062098e74033a82b2d9c268
seandavidreed <seanreed7992@gmail.com>
2022-11-04 00:32:23 -0700

    Add createPDF function; Use EmailMessage class in finalize view; Configure Django SMTP connection; Modify createPDF function to actually create order form

--- mysite/inventory/views.py
+++ mysite/inventory/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, FileResponse
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 from django.urls import reverse
 import io
 from reportlab.pdfgen import canvas
@@ -31,21 +31,83 @@ def take_inventory(request):
         return HttpResponseRedirect(reverse(next_page))
     return render(request, 'inventory/take-inventory.html', {'item_list': item_list})
 
+
+def createPDF():
+    # Fetch database objects by date
+    things = Order.objects.filter(date=datetime.datetime.now(), item__supplier__name='Cash and Carry')
+
+    # Create Bytestream buffer
+    buffer = io.BytesIO()
+    b = True
+
+    # Create a canvas
+    p = canvas.Canvas(buffer, pagesize=letter, bottomup=0)
+
+    for thing in things:
+        if b is True:
+            # Create a text object
+            textob = p.beginText()
+            textob.setTextOrigin(inch, inch)
+
+            # Create Letterhead
+            textob.setFont("Courier", 18)
+            textob.textLine("Woodland Espresso")
+            p.setLineWidth(0.05*inch)
+            p.line(1*inch, 1.1*inch, 7.5*inch, 1.1*inch)
+
+            # Display order date
+            textob.setFont("Courier", 12)
+            textob.textLine("Order Placed: " + str(datetime.datetime.now()))
+            textob.textLine()
+
+            # Table Headers (currently hard-coded: probably should change)
+            textob.setFont("Courier-Bold", 12)
+            textob.textOut("Item")
+            textob.moveCursor(5*inch, 0)
+            textob.textOut("Order Qty")
+            textob.moveCursor(-5*inch, 0)
+            textob.textLine()
+            b = False
+
+        # Populate document with data from database
+        textob.setFont("Courier", 12)
+        textob.textOut(str(thing.item))
+        textob.moveCursor(5*inch, 0)
+        textob.textOut(str(thing.order_qty))
+        textob.moveCursor(-5*inch, 0)
+        textob.textLine()
+        if textob.getCursor()[1] >= 712:
+            b = True
+            p.drawText(textob)
+            p.showPage()
+
+    # Render PDF document
+    p.drawText(textob)
+    p.showPage()
+    p.save()
+
+    buffer.seek(0)
+    pdf = buffer.getvalue()
+    return pdf
+
+
 @login_required
 def finalize(request):
     supplier_list = Supplier.objects.get(pk=2)
-    order_form = Order.objects.filter(supplier=supplier_list.name)
     if request.method == "POST":
         message = request.POST['message']
-        send_mail(
-            'Order Form',
-            message,
-            'woodlandespresso@gmail.com',
-            [supplier_list.email],
-            fail_silently=False,
+        pdf = createPDF()
+        email = EmailMessage(
+            subject='Order Form',
+            body=message,
+            from_email='seanreed7992@gmail.com',
+            to=[supplier_list.email],
         )
+        email.attach('orderform.pdf', pdf, 'application/pdf')
+        email.send(fail_silently=False)
         return HttpResponseRedirect(reverse('inventory:success'))
-    return render(request, 'inventory/finalize.html', {'supplier_list': supplier_list, 'order_form': order_form})
+    return render(request, 'inventory/finalize.html', {'supplier_list': supplier_list})
+
 
 @login_required
 def success(request):
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,4 +134,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 
 # For sending test emails in development
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+env = environ.Env()
+environ.Env.read_env()
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+
+RECIPIENT_ADDRESS = env('RECIPIENT_ADDRESS')
